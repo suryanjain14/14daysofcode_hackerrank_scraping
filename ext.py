821362b539919fdb61d5821c2afad72fa4dac02e
Suryan Jain <suryanjain14@gmail.com>
2019-10-15 23:50:14 +0530

    completed extracting both times and name file writing completed day12 attendance completed

--- ext.py
+++ ext.py
@@ -12,23 +12,27 @@ driver.find_element_by_xpath(
 driver.find_element_by_xpath(
     '/html/body/div[9]/div/div/div/section/div/div/div[2]/div[1]/form/div[2]/div/div/input').send_keys("nh!234567")
 driver.find_element_by_xpath("/html/body/div[9]/div/div/div/section/div/div/div[2]/div[1]/form/div[4]/button").click()
-# Enter the no of pages on pagination file
-pageno = 6
+# Enter the no of pages in pagination
+pageno = 100
 
 # Name the file
-file = open("restaurant.csv", "w")
+file = open("small-triangles-large-triangles.csv", "w")
 file.write("Name,Time\n")
 for page in range(1, pageno + 1):
     # Enter the link of contests leaderboard below address field
-    address = 'https://www.hackerrank.com/contests/14daysofcode-beginner/challenges/restaurant/leaderboard/'\
+    address = 'https://www.hackerrank.com/contests/1st-year-12-days-coding-challenge/challenges/small-triangles-large-triangles/leaderboard/' \
               + str(page)
 
     driver.get(url=address)
     driver.implicitly_wait(8)
     # driver.implicitly_wait(40)
     # driver.find_element_by_xpath('//*[@id="legacy-signup"]/div[1]/p/button').click()
+
+    # variables for names
     firstn = '/html/body/div[2]/div[10]/div/div/section/div/div[2]/section/div[2]/div[2]/div['
     secondn = ']/div/div[2]/p/a'
+
+    # variables for time
     firstT = '/html/body/div[2]/div[10]/div/div/section/div/div[2]/section/div[2]/div[2]/div['
     secondT = ']/div/div[5]/p'
 
